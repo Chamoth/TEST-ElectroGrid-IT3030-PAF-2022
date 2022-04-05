591833f60ddfe879ee9342bb9b99c2044919d8c1
CHAMOTH <cketipearachchi@gmail.com>
2022-04-05 13:00:35 +0530

    first commit

---
+++ src/main/java/com/electrogrid/generate_bills_service/GenerateBillsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.electrogrid.generate_bills_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateBillsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerateBillsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/electrogrid/generate_bills_service/GenerateBillsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electrogrid.generate_bills_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateBillsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
